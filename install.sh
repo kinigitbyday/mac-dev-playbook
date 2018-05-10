7c1148b0fdf62b40e702aaf6be4af3f99c75b3dd
Reid Knight <knightr565@gmail.com>
2018-05-10 15:30:45 -0700

    Add zsh and yadr

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Usage:
+# install.sh
+
+if [[ -z $(which brew) ]]; then
+  echo "Installing Homebrew...";
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null;
+fi
+
+if [[ -z $(which ansible) ]]; then
+    echo "Installing Ansible";
+    brew install ansible > /dev/null;
+fi
+
+WHOAMI=$(whoami);
+
+if [[ -d "/Users/${WHOAMI}/Documents/dotfiles" ]]; then
+    echo "Removing dotfiles";
+    rm -rf "/Users/${WHOAMI}/Documents/dotfiles" > /dev/null;
+fi
+if [[ -d "/Users/${WHOAMI}/.setup" ]]; then
+    echo "Removing playbook";
+    rm -rf "/Users/${WHOAMI}/.setup" > /dev/null;
+fi
+
+git clone https://github.com/fubarhouse/mac-dev-playbook.git "/Users/${WHOAMI}/.setup" > /dev/null;
+git clone https://github.com/fubarhouse/mac-dev-playbook-dotfiles.git "/Users/${WHOAMI}/Documents/dotfiles" > /dev/null;
+
+cd "/Users/${WHOAMI}/.setup/";
+
+echo "Installing requirements";
+ansible-galaxy install -r ./requirements.yml;
+
+echo "Initiating playbook";
+
+ansible-playbook ./main.yml -i inventory -U $(whoami) --ask-sudo-pass;
+
+echo "Done.";
