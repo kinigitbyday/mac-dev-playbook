89245536ca1fede832a19980f15484b39ee1705e
Reid Knight <knightr565@gmail.com>
2018-05-29 16:35:24 -0700

    Remove yadr in print statments

--- vimrc.sh
+++ vimrc.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Install YADR
-if [ -d "./.yadr" ]; then
-  echo "YADR repo dir exists. Removing ..."
+if [ -d "./.vim_runtime" ]; then
+  echo "vim_runtime repo dir exists. Removing ..."
   rm -rf ./.vim_runtime
 fi
 
-echo "YADR rake install..."
+echo "vim_runtime install..."
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 
 sh ~/.vim_runtime/install_awesome_vimrc.sh
