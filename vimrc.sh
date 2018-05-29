c553373dab3a423647e67f6193cc54d72d7fb08f
Reid Knight <knightr565@gmail.com>
2018-05-29 16:33:17 -0700

    Switch out yadr with amix vimrc

--- vimrc.sh
+++ vimrc.sh
@@ -3,11 +3,10 @@
 # Install YADR
 if [ -d "./.yadr" ]; then
   echo "YADR repo dir exists. Removing ..."
-  rm -rf ./.yadr
+  rm -rf ./.vim_runtime
 fi
 
 echo "YADR rake install..."
-git clone https://github.com/kinigitbyday/yadr.git ~/.yadr
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 
-cd ~/.yadr
-rake install
+sh ~/.vim_runtime/install_awesome_vimrc.sh
