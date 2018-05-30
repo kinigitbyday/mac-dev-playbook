b2d742884075f4cc360e0d0794a8e96f2090bf86
Reid Knight <knightr565@gmail.com>
2018-05-29 18:37:06 -0700

    Add amix vimrc, remove yadr, add karabiner elements (#1)
    * Test removing caskroom/cask
    
    * Add karabiner
    
    * Switch out yadr with amix vimrc
    
    * Remove yadr in print statments
    
    * Remove yadr in comments
    
    * Comment out chrome due to broken checksum, add caskroom/versions tap
    
    * Use other name for versions tap
    
    * Add karabiner-elements in place of karabiner

---
+++ vimrc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install vim_runtime
+if [ -d "./.vim_runtime" ]; then
+  echo "vim_runtime repo dir exists. Removing ..."
+  rm -rf ./.vim_runtime
+fi
+
+echo "vim_runtime install..."
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+
+sh ~/.vim_runtime/install_awesome_vimrc.sh
